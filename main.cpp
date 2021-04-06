035185681eabec073a28f8e9f3435a5fac9ae750
st150018 <st150018@stud.uni-stuttgart.de>
2021-04-06 12:46:58 +0200

    Spielfeld zeichnen (Funktion definiert)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+char obenLinks = '1';
+char obenMitte = '2';
+char obenRechts = '3';
+char mitteLinks='4';
+char mitteMitte = '5';
+char mitteRechts = '6';
+char untenLinks = '7';
+char untenMitte= '8';
+char untenRechts ='9';
+
+void board()
+{
+    //system("cls");
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Player 1 (X)  vs. Player 2 (O)" << std::endl;
+    //cout << "Player 1 (X)  -  Player 2 (O)" << endl << endl;
+    std::cout << "     |     |     "  << std::endl;
+    std::cout << "  " << obenLinks<< "  |  " << obenMitte << "  |  " << obenRechts<< std::endl;
+    std::cout <<  "_____|_____|_____" << std::endl;
+    std::cout << "     |     |     " << std::endl;
+    std::cout << "  " << mitteLinks<< "  |  " <<mitteMitte << "  |  " << mitteRechts << std::endl;
+    std::cout << "_____|_____|_____" <<  std::endl;
+    std::cout << "     |     |     " <<  std::endl;
+    std::cout << "  " << untenRechts << "  |  " << untenMitte << "  |  " << untenRechts << std::endl;
+    std::cout << "     |     |     " <<std::endl;
+}
+
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "init commit : "<<std::endl;
+    board();
     return 0;
 }
