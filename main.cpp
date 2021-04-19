fac95639be258fda9aca517daedc157c6f1d4eb3
st150018 <st150018@stud.uni-stuttgart.de>
2021-04-19 14:46:17 +0200

    second chapter

--- main.cpp
+++ main.cpp
@@ -3,21 +3,32 @@
 #include <string>
 using namespace std;
 // ------------------- Ende: Einstellungen ---------------------------------
-void zeichneSpielfeld(){
-    cout << "Spieler 1 (X)  gegen Spieler 2 (O)" << endl;
+
+char obenLinks = '1';
+char obenMitte = '2';
+char obenRechts = '3';
+char mitteLinks='4';
+char mitteMitte = '5';
+char mitteRechts = '6';
+char untenLinks = '7';
+char untenMitte= '8';
+char untenRechts ='9';
+
+void zeichneSpielfeld()
+{
+    cout << "Player 1 (X)  vs. Player 2 (O)" << std::endl;
     cout << "     |     |     "  << endl;
-    cout << "  " << " "<< "  |  " << " "<< "  |  " << " "<< endl;
+    cout << "  " << obenLinks<< "  |  " << obenMitte << "  |  " << obenRechts<< endl;
     cout <<  "_____|_____|_____" << endl;
     cout << "     |     |     " << endl;
-    cout << "  " << " "<< "  |  " <<" " << "  |  " << " " << endl;
+    cout << "  " << mitteLinks<< "  |  " <<mitteMitte << "  |  " << mitteRechts << endl;
     cout << "_____|_____|_____" <<  endl;
     cout << "     |     |     " <<  endl;
-    cout << "  " << " " << "  |  " << " " << "  |  " << " " << endl;
-    cout << "     |     |     " <<endl;
+    cout << "  " << untenLinks << "  |  " << untenMitte << "  |  " << untenRechts << endl;
+    cout << "     |     |     " <<std::endl;
 }
 
-
-int main() {
+int main(){
     zeichneSpielfeld();
     return 0;
 }
